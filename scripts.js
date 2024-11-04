040fe22006355e5c33acf157d4ac705a293adf91
calil-vaz <calilvazdev@gmail.com>
2024-11-04 15:04:30 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -336,21 +336,6 @@ function generatePDF() {
   // Exibe o conteúdo para o PDF
   document.getElementById("content").style.display = "block";
   const element = document.getElementById("content");
-  /*
-  html2pdf()
-    .set({
-      margin: [0, 0, 0, 0],
-    })
-    .from(element)
-    .save(
-      `Loja ${filial.value}_Relatório de mau uso_${requiredInputs[4].value}.pdf`
-    );
-  mostrarModal();
-  setTimeout(function () {
-    document.getElementById("content").style.display = "none";
-    window.location.reload();
-  }, 7000);
-  */
   mostrarModal();
   html2pdf().set({
     margin: [0, 0, 0, 0],
@@ -359,7 +344,6 @@ function generatePDF() {
   .save(`Loja ${filial.value}_Relatório de mau uso_${requiredInputs[4].value}.pdf`)
   .then(() => {
     // Ocultar o modal ao finalizar o download
-    document.getElementById('modal').style.display = 'none';
     window.location.reload();
 }).catch(error => {
     console.error("Erro ao gerar o PDF:", error);
